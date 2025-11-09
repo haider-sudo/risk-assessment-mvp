be4a5b3bca50df54fc0ea0d89cb49409509d14f6
haider-sudo <m.haiderchaudhary.6@gmail.com>
2025-11-09 16:04:49 +0100

    Render-url

--- client/src/components/AdminPage.js
+++ client/src/components/AdminPage.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://risk-assessment-mvp.onrender.com';
 
 function AdminPage({onBack}) {
  const [requests, setRequests] = useState([]);
